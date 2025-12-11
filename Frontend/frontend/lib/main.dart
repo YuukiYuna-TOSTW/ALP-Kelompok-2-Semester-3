a17ea7f45aff23c638143fd68e12be8c3451610b
theofany blessing gladysia purwanta <teofannypurwanta@gmail.com>
2025-12-11 12:31:53 +0800

    Simpan data sebelum perbaikan

--- Frontend/frontend/lib/main.dart
+++ Frontend/frontend/lib/main.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'config/theme/app_theme.dart';
-
-import 'features/layout/layout_template.dart';
-import 'features/dashboard/pages/dashboard_page.dart'; // FIXED PATH
-=======
 //import 'features/homepage/pages/homepage.dart';
 //import 'features/homepage/pages/tambah_kegiatan.dart';
 import 'features/homepage/pages/otp_verification_screen.dart';
 //import 'features/homepage/pages/otp_verification_screen.dart';
 // import 'features/homepage/pages/otp_verification_screen.dart';
 // import '../../../widgets/otp_timer.dart';
->>>>>>> autentikasi_login
 
 void main() {
   runApp(const MyApp());
@@ -25,32 +18,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SMPN 1 Bontonompo Selatan',
-<<<<<<< HEAD
-
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.system, // Dark mode otomatis
-      home: const RootPreview(),
-    );
-  }
-}
-
-class RootPreview extends StatelessWidget {
-  const RootPreview({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const LayoutTemplate(
-      userName: 'Admin SMPN 1 Bontonompo Selatan',
-      role: 'admin',
-      child: DashboardHome(), // FIXED
-=======
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.white,
         primaryColor: const Color(0xFF75CFFF),
       ),
       home: OtpVerificationPage(),
->>>>>>> autentikasi_login
     );
   }
 }
--- Frontend/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Frontend/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -4,16 +4,8 @@
 
 import FlutterMacOS
 import Foundation
-
-<<<<<<< HEAD
-import path_provider_foundation
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-=======
 import file_picker
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
->>>>>>> autentikasi_login
 }
